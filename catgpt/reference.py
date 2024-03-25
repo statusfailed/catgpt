bfc1305bc377ffa039a36864e3aae196c91231c4
statusfailed <paul@statusfailed.com>
2024-03-25 16:03:30 +0000

    make reference functions take dims argument

--- catgpt/reference.py
+++ catgpt/reference.py
@@ -2,21 +2,27 @@
 from catgpt.util import product
 
 # not sure what internal impl. is. Hope this works!
-def mean(x):
-    n_dim = product(x.shape)
-    return x.sum() / n_dim
+def mean(x, dims):
+    n_dim = product(x[dims].shape)
+    return x.sum(dims) / n_dim
 
-def r_mean(x, dy):
-    N = product(x.shape)
+def r_mean(x, dy, dims):
+    N = product(x[dims].shape)
     return (1/N)*dy
 
 # reference implementation according to docs
 # https://pytorch.org/docs/stable/generated/torch.var.html
-def var(x, correction=1):
-    N = product(x.shape)
-    m = mean(x)
+def var(x, dims, correction=1):
+    N = product(x[dims].shape)
+
+    # This is a torch one-liner, but catgrad core will require explicit broadcasting
+    # m = x.mean(dims, keepdim=True)
+    m = mean(x, dims) # (N₀ N₁ ..., T₀, T₁, ...)
+    m = m.view(tuple(m.shape + (1,)*len(dims))) # broadcast to (N₀, N₁, ..., 1, 1, ..., 1)
+
+    var_sum = ((x - m)**2).sum(dims)
     d = max(0, N - correction)
-    return (1 / d) * ((x - m)**2).sum()
+    return var_sum / d
 
 # https://math.stackexchange.com/questions/2836083/derivative-of-the-variance-wrt-x-i#2887894
 def r_var(x, dy, correction=1):
--- tests/test_reference.py
+++ tests/test_reference.py
@@ -2,35 +2,38 @@
 import torch
 from catgpt.reference import mean, r_mean, var, r_var
 
-def test_op_mean():
-    shape = (100, 100, 100)
+def test_reference_mean():
+    shape = (100, 100, 100, 100)
+    dims = (-2, -1)
     x = torch.normal(10, 2, shape)
 
-    expected = x.mean()
-    actual   = mean(x)
+    expected = x.mean(dims)
+    actual   = mean(x, dims=dims)
     assert torch.allclose(expected, actual)
 
-def test_op_r_mean():
-    shape = (100, 100, 100)
+def test_reference_r_mean():
+    shape = (100, 100, 100, 100)
+    dims = (-2, -1)
     x = torch.normal(10, 2, shape, requires_grad=True)
 
-    dy = torch.tensor(1)
-    expected = x.mean().grad_fn(dy)
-    actual   = r_mean(x, dy)
+    dy = torch.ones((100,100))
+    expected = x.mean(dims).grad_fn(dy)
+    actual   = r_mean(x, dy, dims=dims)
     assert torch.allclose(expected, actual)
 
 
 # see https://pytorch.org/docs/stable/generated/torch.var.html
-def test_op_var():
-    shape = (100, 100, 100)
+def test_reference_var():
+    shape = (100, 100, 100, 100)
+    dims = (-2, -1)
     x = torch.normal(10, 2, shape)
 
-    expected = x.var()
-    actual   = var(x)
+    expected = x.var(dims)
+    actual   = var(x, dims)
     assert torch.allclose(expected, actual)
 
 
-def test_op_r_var():
+def test_reference_r_var():
     shape = (100, 100, 100)
     x = torch.normal(10, 2, shape, requires_grad=True)
 
